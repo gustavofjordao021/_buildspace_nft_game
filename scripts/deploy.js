2cf6183f6e64d805a7b741e57fecd91b3f7f3124
Gustavo Jordão <gustavo.jordao021@gmail.com>
2021-11-20 09:51:39 -0500

    Redeploying images with standard sizes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,16 +3,16 @@ const main = async () => {
   const gameContract = await gameContractFactory.deploy(
     ["Uzumaki Naruto", "Uchiha Sasuke", "Hatake Kakashi"], // Names
     [
-      "https://static.wikia.nocookie.net/boruto/images/9/9d/375px-NarutoBorutoMovie.jpg", // Images
-      "https://static.wikia.nocookie.net/boruto/images/b/b7/Sasuke_Epilogue.png",
-      "https://s4.anilist.co/file/anilistcdn/character/large/b85-mkVBh2yjxjmx.png",
+      "https://static.wikia.nocookie.net/naruto/images/4/42/Naruto_Part_III.png", // Images
+      "https://static.wikia.nocookie.net/naruto/images/b/b7/Sasuke_Part_3.png",
+      "https://static.wikia.nocookie.net/naruto/images/2/27/Kakashi_Hatake.png",
     ],
     [200, 150, 120], // HP values
     [200, 150, 120], // MaxHP values
     [120, 100, 80], // Chakra values
     [50, 50, 40], // Attack damage values
     "Ōtsutsuki Kaguya", // Boss name
-    "https://static.wikia.nocookie.net/naruto/images/6/6c/Kaguya_%C5%8Ctsutsuki.png/revision/latest/scale-to-width-down/2000?cb=20180824113908", // Boss image
+    "https://static.wikia.nocookie.net/naruto/images/6/6c/Kaguya_%C5%8Ctsutsuki.png", // Boss image
     1000, // Boss hp
     500, // Boss chakra
     50 // Boss attack damage
